831f664b76fe566d2694dcc5e7da72708365e0bf
majiaxin110 <majiaxin110@gmail.com>
2020-07-27 16:26:49 +0800

    完善登录注册API文档

--- src/main/java/org/aidtracker/backend/web/config/Swagger2.java
+++ src/main/java/org/aidtracker/backend/web/config/Swagger2.java
@@ -6,9 +6,12 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 /**
  * @author MT
@@ -30,8 +33,8 @@ public class Swagger2 {
         return new ApiInfoBuilder()
                 .title("AidTracker API")
                 .description("A backend project that provides api services for aid tracker wechat mini program and web page\n" +
-                        "文档：https://shimo.im/docs/8wgRQpryKCPQRqJy/")
-                .termsOfServiceUrl("https://github.com/majiaxin110/aid-tracker-backend")
+                        "文档：https://shimo.im/docs/8wgRQpryKCPQRqJy/\n" +
+                        "<a href=\"https://github.com/majiaxin110/aid-tracker-backend\">Github Repository</a>")
                 .version("0.0.1")
                 .build();
     }
--- src/main/java/org/aidtracker/backend/web/controller/AuthController.java
+++ src/main/java/org/aidtracker/backend/web/controller/AuthController.java
@@ -1,9 +1,11 @@
 package org.aidtracker.backend.web.controller;
 
+import io.swagger.annotations.*;
 import org.aidtracker.backend.util.SimpleResult;
 import org.aidtracker.backend.web.dto.AccountDTO;
 import org.aidtracker.backend.web.dto.AccountRegisterRequest;
 import org.aidtracker.backend.web.service.AccountService;
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +19,7 @@ import java.util.Objects;
  * @since 2020/7/27 11:13
  */
 @RestController
+@Api("认证登录")
 public class AuthController {
     @Autowired
     private AccountService accountService;
@@ -24,7 +27,13 @@ public class AuthController {
     public static final String NEW_USER = "NEW_USER";
 
     @PostMapping("/auth/login")
-    public SimpleResult<?> tryLogin(@RequestParam String jsCode) {
+    @ApiOperation(value = "登录",
+            notes = "登录注册流程参考上方文档及微信小程序登录流程 https://developers.weixin.qq.com/miniprogram/dev/framework/open-ability/login.html"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "新用户初次登录返回\"NEW_USER\"")
+    })
+    public SimpleResult<?> tryLogin(@RequestParam @ApiParam("调用wx.login()产生的临时登录凭证Id") String jsCode) {
         AccountDTO result = accountService.tryLogin(jsCode);
         if (Objects.isNull(result)) {
             return SimpleResult.success(NEW_USER);
@@ -33,6 +42,7 @@ public class AuthController {
     }
 
     @PostMapping("/auth/register")
+    @ApiOperation(value = "注册")
     public SimpleResult<AccountDTO> tryRegister(@RequestBody AccountRegisterRequest request) {
         return SimpleResult.success(accountService.tryRegister(request));
     }
